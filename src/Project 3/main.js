76d5e4c947315e5b2b23c328d7345d4e2364386b
Malachi I. Beerram <malachi.beerram@centre.edu>
2021-10-21 17:50:58 -0400

    (87/87 mins on 10/21/2021) Lessons 3: Grid share on Desktop and Mobile

---
+++ src/Project 3/main.js
@@ -0,0 +1,12 @@
+
+// window.onresize = screen;
+// window.onload = screen;
+
+// function screen() {
+//     myWidth =  window.innerWidth;
+//     myHeight =  window.innerHeight;
+
+//     document.getElementById("size").innerHTML
+//     = " Width : " + myWidth + "px and Height :" + myHeight +"px";
+    
+// }
